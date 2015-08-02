c83691322233005170784b109d0266c52eee5c1a
Richard Buckley <richard.buckley@ieee.org>
2015-08-02 14:47:33 +0000

    Change debugging output to Console output.

--- src/PDP8.h
+++ src/PDP8.h
@@ -4,12 +4,13 @@
 #define _PDP8_H_
 
 #include <stdio.h>
+#include "Console.h"
 
 #ifndef DEBUG_LEVEL
 #define DEBUG_LEVEL 0
 #endif
 
 // Requires fmt be a string literal
-#define debug(l,fmt,...) do{if(l<DEBUG_LEVEL) fprintf(stderr, "%s: " fmt, __PRETTY_FUNCTION__, __VA_ARGS__);} while(0)
+#define debug(l,fmt,...) do{if(l<DEBUG_LEVEL) Console::instance()->printf( "%s: " fmt, __PRETTY_FUNCTION__, __VA_ARGS__);} while(0)
 
 #endif	// _PDP8_H_
