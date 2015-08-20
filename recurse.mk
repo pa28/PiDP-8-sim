d8c304241e273cd3d399edefb18569964c59e08f
Richard Buckley <richard.buckley@ieee.org>
2015-08-20 17:49:18 -0400

    Global project makefile settings.

---
+++ recurse.mk
@@ -0,0 +1,10 @@
+
+ROOT_DIR=/opt/pidp8
+BIN_DIR=$(ROOT_DIR)/bin
+ETC_DIR=$(ROOT_DIR)/etc
+
+INSTALL=/usr/bin/install
+INSTALL_OPTS=-o root -g root
+INSTALL_DOPTS=-m 0755
+INSTALL_XOPTS=-s -m 0755
+INSTALL_FOPTS=-m 0644
