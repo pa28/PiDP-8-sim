d20a71b62ee1b4dba150cca6ffea4de33b491d6d
richard <richard@hrbuckley.net>
2018-01-27 15:00:55 -0500

    Changes to bring coding up-to-date.

--- Memory.h
+++ Memory.h
@@ -14,36 +14,20 @@
 #include <stack>
 #include <utility>
 #include <exception>
+#include <array>
+#include <memory>
+#include <cmath>
+#include "Registers.h"
 #include "Device.h"
-#include "CPU.h"
 
-namespace pdp8
-{
+namespace hw_sim {
 
-    enum MemoryConstants {
-        MAXMEMSIZE = 8 * 4096,
-    };
+    constexpr size_t MAXMEMSIZE = 8 * 4096;
 
 
-    enum MemoryExceptionCode {
-        MemoryExcpetionMaxSize = 0
-    };
-
-
-    class MemoryException : public std::exception {
+    class MemoryOutOfRange : public std::out_of_range {
     public:
-        MemoryException(MemoryExceptionCode c) : code(c) {}
-        virtual ~MemoryException() throw() {}
-        virtual const char* what() const throw() {
-            switch (code) {
-                case MemoryExcpetionMaxSize:
-                    return "Memory request beyond maximum possable memory size.";
-                default:
-                    return "Unknown memory error.";
-            }
-        }
-    protected:
-        MemoryExceptionCode	code;
+        MemoryOutOfRange() : std::out_of_range("Memory access out of range") {}
     };
 
     enum MemoryFlag {
@@ -53,81 +37,168 @@ namespace pdp8
         MemFlagBreakRead = 4,
         MemFlagBreakWrite = 8,
         MemFlagBreak = MemFlagBreakExecute | MemFlagBreakRead | MemFlagBreakWrite,
-        MemFlagRange = 16,
-        MemFlagMaxSize = 32,
     };
 
+    /**
+     * An stream inserter for a MemoryFlag
+     * @tparam Ct The character type of the stream
+     * @tparam Tt The character traits type of the stream
+     */
+    template<class Ct, class Tt>
+    std::basic_ostream<Ct, Tt> &operator<<(std::basic_ostream<Ct, Tt> &os, MemoryFlag f) {
+        return os << setw(1)
+                  << setfill(' ')
+                  << (f & MemFlagInitialized ? 'I' : '-')
+                  << (f & MemFlagBreakExecute ? 'X' : '-')
+                  << (f & MemFlagBreakRead ? 'R' : '-')
+                  << (f & MemFlagBreakWrite ? 'W' : '-');
+    };
+
+    constexpr size_t MemoryFlagWidth = 4;
+
+    template<typename Base, size_t Width>
     class MemoryCell {
-        friend class Memory;
-
-        MemoryCell(int v = 0);
-
     public:
-        virtual ~MemoryCell();
+        MemoryCell() {
+            clear();
+        }
 
-        //operator int();
-        //operator uint16_t();
-        operator int32_t() { return (int32_t)(mb = m); }
-        //operator unsigned int() { return (unsigned int) operator int(); }
+        constexpr static size_t width() { return Width; }
 
-        int operator = (int v);
+        using base_type = Base;
 
-        bool operator == (MemoryCell & other) { return m == other.m; }
+        MemoryCell &operator=(Base v) {
+            static_assert(sizeof(Base) * 8 >= Width + MemoryFlagWidth);
+            m = (m & ~hw_sim::RegisterMask<Width>::value)
+                | (v & hw_sim::RegisterMask<Width>::value);
+            return *this;
+        }
 
-        operator bool() { return m != -1; }
+        explicit operator Base() const {
+            static_assert(sizeof(Base) * 8 >= Width + MemoryFlagWidth);
+            return static_cast<Base>(m & hw_sim::RegisterMask<Width>::value);
+        }
+
+        Base operator()() const {
+            static_assert(sizeof(Base) * 8 >= Width + MemoryFlagWidth);
+            return static_cast<Base>(*this);
+        }
+
+        bool operator==(MemoryCell &other) {
+            static_assert(sizeof(Base) * 8 >= Width + MemoryFlagWidth);
+            return static_cast<Base>(*this) == static_cast<Base>(other);
+        }
+
+        MemoryFlag flags() const {
+            return static_cast<MemoryFlag>((m >> Width) & hw_sim::RegisterMask<MemoryFlagWidth>::value);
+        }
+
+        void setFlag(MemoryFlag f) {
+            m |= f << Width;
+        }
+
+        void clear() {
+            static_assert(sizeof(Base) * 8 >= Width + MemoryFlagWidth);
+            m = MemFlagClear << Width;
+        }
 
     protected:
-        uint16_t    m;
-        static uint16_t     mb;
-
-        void    clear() { m = 0; }
+        Base m;
     };
 
-    class Memory: public Device
+    /**
+     * An stream inserter for a memory cell
+     * @tparam Ct The character type of the stream
+     * @tparam Tt The character traits type of the stream
+     * @tparam Base The cell base type
+     * @param Width The cell width
+     * @return The stream object
+     */
+    template<class Ct, class Tt, typename Base, size_t Width>
+    std::basic_ostream<Ct, Tt> &operator<<(std::basic_ostream<Ct, Tt> &os, const MemoryCell<Base, Width> &m) {
+        double bits = 0;
+        int radix = 0;
+        auto basefield = os.flags() & std::ios_base::basefield;
+        switch (basefield) {
+            case std::_S_oct:
+                bits = 3;
+                radix = 8;
+                break;
+            case std::_S_hex:
+                bits = 4;
+                radix = 16;
+                break;
+            default:
+                bits = 3.322;
+                radix = 10;
+        }
+
+        double intPart;
+        double fracPart = modf(Width / bits, &intPart);
+
+        auto w = static_cast<int>(intPart) + (fracPart != 0 ? 1 : 0);
+
+        return os << std::setbase(radix)
+                  << std::setw(w)
+                  << std::setfill('0')
+                  << m();
+    };
+
+    template<size_t Size, typename Base, size_t Width>
+    class Memory //: public Device
     {
-        Memory();
-
     public:
-        virtual ~Memory();
+        Memory() : /*Device("MEM", "Core Memory"),*/ ma(), mb(), m() {}
 
-        static Memory * instance();
+        constexpr static size_t size() { return Size; }
 
-        virtual void initialize() {}
-        virtual void reset() {}
-        virtual void stop() {}
+        constexpr static size_t width() { return Width; }
 
-        //MemoryCell & operator [] (int ma) throw(MemoryException);
-        MemoryCell & operator [] (int32_t ma) throw(MemoryException);
+        using base_type = Base;
 
-        bool flagSet() { return ! flagStack.empty(); }
-        int flagStackSize() { return flagStack.size(); }
-        void clearFlackStack() { while (!flagStack.empty()) flagStack.pop(); }
+        MemoryCell<Base, Width> &operator[](size_t memoryAddress) {
+            return at(memoryAddress);
+        }
 
-        static uint16_t     MB() { return MemoryCell::mb; }
-        static uint16_t     MA() { return ma; }
+        MemoryCell<Base, Width> &at(size_t memoryAddress) {
+            if (memoryAddress < size()) {
+                ma = begin() + memoryAddress;
+                mb = *ma;
+                return *ma;
+            }
+            throw MemoryOutOfRange{};
+        }
 
-        int     loadFile(const char *fileName);
-        int     saveFile(const char *fileName, int32_t start, int32_t end);
-        void	initMemory(const int32_t *mem, int32_t start, int32_t end, int32_t startAddr, bool run = false);
+        MemoryCell<Base, Width> &MB() { return mb; }
+
+        auto MA() { return ma; }
+
+        auto begin() { return m.begin(); }
+
+        auto end() { return m.end(); }
+
+        auto cbegin() const { return m.cbegin(); }
+
+        auto cend() const { return m.cend(); }
+
+        auto rbegin() { return m.rbegin(); }
+
+        auto rend() { return m.rend(); }
+
+        auto crbegin() const { return m.crbegin(); }
+
+        auto crend() const { return m.crend(); }
+
+        void clear() {
+            for (auto i = begin(); i != end(); ++i)
+                i->clear();
+        }
 
     protected:
+        typename std::array<MemoryCell<Base, Width>, Size>::iterator ma;
+        MemoryCell<Base, Width> mb;
 
-        static Memory *		_instance;
-        static MemoryFlag   flags[];
-        static MemoryCell   errCell, m[];
-        static uint16_t     ma;
-
-        int     memorySize;
-        std::stack < std::pair < uint32_t, int > >  flagStack;
-        bool	exceptionOn;
-
-        int     sim_load_bin( FILE * fp );
-        int32_t    sim_bin_getc (FILE *fi, uint32_t *newf);
-
-        void    clearMemory();
-
-        uint32_t	lastOrigin;
-        uint32_t	lastField;
+        std::array<MemoryCell<Base, Width>, Size> m;
     };
 
 } /* namespace pdp8 */
