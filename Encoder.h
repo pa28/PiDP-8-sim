3985b25817bacb7b7cd292211b3f489f97430e09
richard <richard@hrbuckley.net>
2018-02-03 12:15:37 -0500

    Implement decoding.

--- Encoder.h
+++ Encoder.h
@@ -8,10 +8,31 @@
 
 #include <streambuf>
 #include <cstring>
+#include <stdexcept>
 
 using namespace std;
 
 namespace util {
+
+    class EncodingError : public std::logic_error {
+    public:
+        EncodingError() = delete;
+
+        explicit EncodingError(const char *w) : std::logic_error(w) {}
+
+        explicit EncodingError(const std::string &w) : std::logic_error(w) {}
+    };
+
+
+    class DecodingError : public std::logic_error {
+    public:
+        DecodingError() = delete;
+
+        explicit DecodingError(const char *w) : std::logic_error(w) {}
+
+        explicit DecodingError(const std::string &w) : std::logic_error(w) {}
+    };
+
 
     template <class CharT, class Traits = std::char_traits<CharT>>
     class Encoder : public std::basic_streambuf<CharT>
@@ -22,7 +43,11 @@ namespace util {
         typedef Traits traits_type;
         typedef typename Traits::int_type int_type;
 
-        explicit Encoder(std::basic_streambuf<CharT> &sb) : _sb(sb), outIdle{true}, inIdle{true} {
+        explicit Encoder(std::basic_streambuf<CharT> &sb) :
+                _sb(sb),
+                outIdle{true},
+                inIdle{true},
+                atEnd{false} {
             this->setp(obuf, obuf+obuf_len);
             this->setg(ibuf, ibuf+putback_len, ibuf+putback_len);
 
@@ -39,7 +64,7 @@ namespace util {
 
         };
 
-        constexpr static size_t buffer_len = 4;
+        constexpr static size_t buffer_len = 64;
         constexpr static size_t putback_len = 8;
         constexpr static size_t obuf_len = buffer_len;
         constexpr static size_t ibuf_len = buffer_len + putback_len;
@@ -50,25 +75,50 @@ namespace util {
         CharT obuf[obuf_len];
         CharT ibuf[ibuf_len];
         std::basic_streambuf<CharT> &_sb;
-        bool outIdle, inIdle;
+        bool outIdle, inIdle, atEnd;
 
         static CharT leader[2];
         static CharT trailer[2];
 
-        int sync() {
-            if (!outIdle) {
-                outIdle = true;
-                if (this->sputc(ETX) == traits_type::eof())
-                    return -1;
-                if (this->sputc(SYN) == traits_type::eof())
-                    return -1;
-            }
 
+    public:
+        bool isAtEnd() const { return (this->gptr() == this->egptr() ? atEnd : false); }
+
+
+        int beginEncoding() {
+            if (!outIdle)
+                throw EncodingError("Begin must be in idle mode.");
+
+            if (this->sputc(SYN) == traits_type::eof())
+                return -1;
+            if (this->sputc(STX) == traits_type::eof())
+                return -1;
+            outIdle = false;
+        }
+
+
+        int endEncoding() {
+            if (outIdle)
+                throw EncodingError("End must not be in idle mode.");
+
+            outIdle = true;
+            if (this->sputc(ETX) == traits_type::eof())
+                return -1;
+            if (this->sputc(SYN) == traits_type::eof())
+                return -1;
+
+            return 0;
+        }
+
+
+    protected:
+        int sync() {
             if (realSync() < 0)
                 return -1;
 
             return _sb.pubsync();
         }
+
 
         int realSync() {
             std::streamsize n = _sb.sputn(obuf, this->pptr() - obuf);
@@ -87,6 +137,7 @@ namespace util {
             return 0;
         }
 
+
         int_type overflow( int_type c ) {
             if (realSync() < 0)
                 return traits_type::eof();
@@ -99,18 +150,12 @@ namespace util {
             return traits_type::to_int_type(c);
         }
 
+
         std::streamsize xsputn(const CharT* s, std::streamsize n) {
             std::streamsize i{};
 
-            if (outIdle) {
-                if (this->sputc(SYN) == traits_type::eof())
-                    return -1;
-                if (this->sputc(STX) == traits_type::eof())
-                    return -1;
-                outIdle = false;
-            }
-
-            outIdle = false;
+            if (outIdle)
+                throw EncodingError("Can not send data in idle mode.");
 
             for (i = 0; i < n; ++i) {
                 CharT e = traits_type::to_char_type(SO);
@@ -148,61 +193,98 @@ namespace util {
             return i;
         }
 
+
+    public:
+        int beginDecodeing() {
+            if (!inIdle)
+                throw DecodingError("Begin must be in idle mode.");
+
+            int_type c;
+            std::streamsize n = _sb.in_avail();
+            while ((c = _sb.sbumpc()) >= 0) {
+                if (c == STX) {
+                    inIdle = false;
+                    return c;
+                }
+
+                if (c != SYN)
+                    throw DecodingError("Data or unexpected protocol in idle mode.");
+            }
+
+            atEnd = false;
+            return c;
+        }
+
+
+        int endDecoding() {
+            if (inIdle)
+                throw DecodingError("End must not be in idle mode.");
+
+            if (atEnd) {
+                atEnd = false;
+                inIdle = true;
+                return 0;
+            }
+
+            int_type c;
+            while ((c = _sb.sgetc()) >= 0) {
+                if (c == ETX) {
+                    inIdle = true;
+                    return c;
+                }
+
+                throw DecodingError("Unexpected data or protocol at end.");
+            }
+
+            return 0;
+        }
+
+
+    protected:
         int_type underflow( ) {
             std::streamsize n = 0;
             bool escape{false};
 
-            while (n == 0) {
-                n = _sb.sgetn(ibuf + putback_len + 1, ibuf_len - putback_len - 1);
+            if (atEnd)
+                return ETX;
 
-                if (n == 0) {
-                    return traits_type::eof();
-                } else if (n < 0) {
-                    return n;
-                }
+            if (inIdle)
+                throw DecodingError("Input can not be in idle mode.");
 
-                this->setg(ibuf, ibuf + putback_len, ibuf + putback_len + n);
+            CharT *put = ibuf + putback_len;
+            CharT *cur = put;
+            CharT *end = ibuf + ibuf_len + putback_len;
 
-                CharT *put = ibuf + putback_len;
-                CharT *cur = put + 1;
-                CharT *end = cur + n;
-
-                while (cur != end) {
-                    if (inIdle) {
-                        inIdle = *cur != STX;
-                    } else {
-                        if (escape) {
-                            *put = *cur;
-                            escape = false;
-                            ++put;
-                        } else {
-                            if (*cur == SO || *cur == SI) {
-                                escape = true;
-                            } else if (*cur == ETX) {
-                                inIdle = true;
-                            } else if (*cur == STX || *cur == SYN) {
-                                return -1;
-                            } else {
-                                *put = *cur;
-                                ++put;
-                            }
-                        }
-                    }
-                    ++cur;
-                }
+            while (put != end && !atEnd) {
+                CharT c = _sb.sbumpc();
+                if (c == traits_type::eof() || c <= 0)
+                    return traits_type::to_int_type(c);
 
                 if (escape) {
-                    CharT c = _sb.sgetc();
-                    if (c == traits_type::eof())
-                        return c;
                     *put = c;
                     ++put;
+                } else {
+                    switch (c) {
+                        case SO:
+                        case SI:
+                            escape = true;
+                            break;
+                        case ETX:
+                            atEnd = true;
+                            break;
+                        case STX:
+                        case SYN:
+                            throw DecodingError("Unexpected protocol in data.");
+                        default:
+                            *put = c;
+                            ++put;
+                    }
                 }
-
-                this->setg(ibuf, ibuf + putback_len, put);
-
-                return traits_type::to_int_type(*(ibuf + putback_len));
             }
+
+            this->setg(ibuf, cur, put);
+
+            return traits_type::to_int_type(*(ibuf + putback_len));
         }
 
     };
