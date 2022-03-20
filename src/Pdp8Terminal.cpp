5c055912ec51d224b05266a709464564a89ad80c
Richard Buckley <richard.buckley@ieee.org>
2022-03-19 20:21:46 -0400

    Remove dead code.

--- src/Pdp8Terminal.cpp
+++ src/Pdp8Terminal.cpp
@@ -38,59 +38,6 @@ namespace sim {
                 printPanel();
             }
         }
-
-//        while (in().rdbuf()->in_avail()) {
-//            auto c = in().get();
-//            std::cout << fmt::format("{:04o}, ", c);
-//        }
-//        std::cout << '\n';
-//
-//        asmbl::Assembler assembler;
-//        std::stringstream sourceCode(std::string{asmbl::PingPong});
-//        assembler.pass1(sourceCode);
-//        sourceCode.clear();
-//        sourceCode.str(std::string{asmbl::PingPong});
-//        std::stringstream binary;
-//        assembler.pass2(sourceCode, binary, std::cout);
-//
-//        TestCPU cpu{};
-//        auto startAddress = cpu.readBinaryFormat(binary);
-//
-//        while (in().rdbuf()->in_avail() > 0)
-//            in().get();
-//
-//        TestCPU::printPanelSilk(terminal);
-//        displayInputBuffer();
-//        cpu.setRunFlag(true);
-//        while (!cpu.getHaltFlag()) {
-//            queryTerminalSize();
-//            cpu.instruction_cycle();
-//            cpu.printPanel(terminal);
-//            if (cpu.getCycleStat() == sim::PDP8I::CycleState::Interrupt) {
-//                cpu.printPanelSilk(terminal);
-//            } else {
-//                auto[read, write, timeout] = select(true, false, timespan.count());
-//                if (read == sim::Terminal::Data || write == sim::Terminal::Data) {
-//                    if (read == sim::Terminal::Data) {
-//                        if (readInput()) {
-//                            auto cmd = getInputBuffer();
-//                            clearInputBuffer();
-//                            if (cmd == "quit")
-//                                break;
-//                        } else {
-//                            auto cmd = getInputBuffer();
-//                            setCursorPosition(24u, 1u);
-//                            out() << fmt::format("\033[0K> {}", cmd);
-//                        }
-//                    }
-//                    std::chrono::microseconds timespan(timeout);
-//                    std::this_thread::sleep_for(timespan);
-//                }
-//            }
-//        }
-//        cpu.setRunFlag(false);
-//        cpu.printPanel(terminal);
-//
     }
 
     void Pdp8Terminal::inputBufferChanged() {
