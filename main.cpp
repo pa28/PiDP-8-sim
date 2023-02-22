1ee125a60a70117aaa64e460da47e255fca16ac4
Richard Buckley <richard.buckley@outlook.com>
2023-02-22 10:13:12 -0500

    Test new terminal management.

--- main.cpp
+++ main.cpp
@@ -5,15 +5,14 @@
 using namespace pdp8;
 
 int main() {
-
-
     signal(SIGCHLD, SIG_IGN);
-    auto terminalSocket = TerminalSocket();
-    terminalSocket.open();
 
     TerminalManager terminalManager{};
 
-    terminalManager.emplace_back(terminalSocket);
+    auto terminalSocket = TerminalSocket();
+    terminalSocket.open();
+
+    terminalManager.push_back(std::make_unique<Pdp8Terminal>(terminalSocket));
 
     while (!terminalManager.empty())
         terminalManager.serviceTerminals();
